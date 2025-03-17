de4fe7e94dd4373c05782fccb957a1d1f253d281
spidercreate <johnsmith.diamon@gmail.com>
2025-03-17 11:18:44 -0400

    Disclaim Check Fix

--- src/components/TosCheck.tsx
+++ src/components/TosCheck.tsx
@@ -1,13 +1,11 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const TOS_STORAGE_KEY = 'mgf_tos_agreed';
-
 export function TosCheck() {
     const navigate = useNavigate();
 
     useEffect(() => {
-        const tosStatus = localStorage.getItem(TOS_STORAGE_KEY);
+        const tosStatus = localStorage.getItem('disclaimerAccepted');
         if (!tosStatus) {
             navigate('/');
         }
